79e405bb8d8cdd7caaa5f58571a961fb3ebdf057
Eric Olaveson <olavesoneric@gmail.com>
2019-09-10 18:07:09 -0500

    challenge 5

---
+++ challenge5.js
@@ -0,0 +1,63 @@
+//CHALLENGE NUMBER 5
+
+// johns bills 124, 48, 268, 180, 42
+// tip 20% < $50
+// tip 15% > 50, < $50
+// tip 10% > 200
+
+
+let johnBills = {
+    billsArr: [124, 48, 268, 180, 42],
+    tips: [],
+    totals: [],
+    tipCalc: function(){
+        for(let i = 0; i < this.billsArr.length; i++){
+            switch(true){
+                case this.billsArr[i] < 50:
+                    this.tips.push(0.2 * this.billsArr[i]);
+                    this.totals.push(0.2 * this.billsArr[i] + this.billsArr[i])
+                    break
+                case this.billsArr[i] >= 50 && this.billsArr[i] <= 200:
+                    this.tips.push(0.15 * this.billsArr[i]);
+                    this.totals.push(0.15 * this.billsArr[i] + this.billsArr[i])
+                    break
+                case this.billsArr[i] > 200:
+                    this.tips.push(0.1 * this.billsArr[i]);
+                    this.totals.push(0.1 * this.billsArr[i] + this.billsArr[i])
+                    break
+            }
+        }
+    }
+}
+
+johnBills.tipCalc()
+
+console.log(johnBills.tips, johnBills.totals)
+
+let markBills = {
+    billsArr: [77, 375, 110, 45],
+    tips: [],
+    totals: [],
+    tipCalc: function(){
+        for(let i = 0; i < this.billsArr.length; i++){
+            switch(true){
+                case this.billsArr[i] < 100:
+                    this.tips.push(0.2 * this.billsArr[i]);
+                    this.totals.push(0.2 * this.billsArr[i] + this.billsArr[i])
+                    break
+                case this.billsArr[i] >= 100 && this.billsArr[i] <= 300:
+                    this.tips.push(0.1 * this.billsArr[i]);
+                    this.totals.push(0.1 * this.billsArr[i] + this.billsArr[i])
+                    break
+                case this.billsArr[i] > 300:
+                    this.tips.push(0.25 * this.billsArr[i]);
+                    this.totals.push(0.25 * this.billsArr[i] + this.billsArr[i])
+                    break
+            }
+        }
+    }
+}
+
+markBills.tipCalc()
+
+console.log(markBills.tips, markBills.totals)
