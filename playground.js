fb867920ffca370f09fabef0c0fe6bae71ab8235
Eric Olaveson <olavesoneric@gmail.com>
2019-09-13 12:34:16 -0500

    learning

--- playground.js
+++ playground.js
@@ -1,11 +1,17 @@
 const x = 1;
 
-function myFunc(){
-    console.log(x);
-    x = x + 1;
-    console.log(x[0]);
-}
+// function myFunc(){
+//     console.log(x);
+//     x = x + 1;
+//     console.log(x[0]);
+// }
 
-myFunc();
+// myFunc();
 
-console.log(x[0])
+// console.log(x[0])
+
+let boo;
+
+if(boo){
+    console.log("boo is true")
+} else { console.log("boo is false") }
