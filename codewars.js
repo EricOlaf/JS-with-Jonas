39402d197e00e4b051ace45e3597cd935a6eb5ff
Eric Olaveson <olavesoneric@gmail.com>
2019-10-01 12:28:50 -0500

    new problem

--- codewars.js
+++ codewars.js
@@ -19,28 +19,71 @@
 
 // return an index of section or -1 if past last section
 // scrollY is a positive integer and sizes is an array of positive integers
-function getSectionIdFromScroll(scrollY,sizes){
-    let total = 0;
-    for(let i = 0; i < sizes.length; i++){
-      if(i === 0){
-        if(scrollY >= 0 && scrollY <= sizes[0]){
-            console.log("hit ind " + i)
-          return 0
-        } else {
-          total += sizes[i];
-          console.log("continue ind " + i)
-          continue;
+// function getSectionIdFromScroll(scrollY,sizes){
+//     let total = 0;
+//     for(let i = 0; i < sizes.length; i++){
+//       if(i === 0){
+//         if(scrollY >= 0 && scrollY <= sizes[0]){
+//             console.log("hit ind " + i)
+//           return 0
+//         } else {
+//           total += sizes[i];
+//           console.log("continue ind " + i)
+//           continue;
+//         }
+//       } else if( scrollY > total && scrollY <= total+sizes[i] ){
+//         console.log("hit ind " + i)
+//         return i
+//       } else {
+//         total += sizes[i]
+//       }
+//     }
+//     return -1
+//  }
+
+//  console.log(getSectionIdFromScroll(299,[300,200,400,600,100]))
+
+//  console.log(getSectionIdFromScroll(30000,[300,200,400,600,100]))
+
+// var orderedCount = function (text) {
+//     let ans = [];
+//     let letterArr = [];
+//     for(let i = 0; i < text.length; i++){
+//         let j = letterArr.indexOf(text[i]);
+//         if(j !== -1){
+//             ans[j][1]++;
+//         }else{
+//             ans.push([text[i], 1]);
+//             letterArr.push(text[i]);
+//         }
+//     }
+//     return ans;
+//   }
+
+//   console.log(orderedCount("abracadabra"))
+
+function par(str){
+    let c, p;
+    c = 0;
+    p = 0;
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "("){
+            c++;
+        }else if(str[i] === ")"){
+            c--;
         }
-      } else if( scrollY > total && scrollY <= total+sizes[i] ){
-        console.log("hit ind " + i)
-        return i
-      } else {
-        total += sizes[i]
-      }
+        if(c < 0){
+            p++;
+            c++;
+        }
     }
-    return -1
- }
+    return c+p
+}
 
- console.log(getSectionIdFromScroll(299,[300,200,400,600,100]))
-
- console.log(getSectionIdFromScroll(30000,[300,200,400,600,100]))
+console.log(par("()"))
+console.log(par("(()"))
+console.log(par("())"))
+console.log(par(")"))
+console.log(par("("))
+console.log(par(")("))
